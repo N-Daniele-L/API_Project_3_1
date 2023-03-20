abd8261f3a4d71bcda26491f59cff4ab37e5036d
N-Daniele-L <daniele.nicolo@condorcet.be>
2023-03-20 19:18:25 +0100

    Fin méthode update et fin options messages

--- src/exercicesJDBC/Gestion_Table_Employé.java
+++ src/exercicesJDBC/Gestion_Table_Employé.java
@@ -1,9 +1,17 @@
 package exercicesJDBC;
 
+import TradUML.Employe;
+import TradUML.Infos;
+import TradUML.Message;
 import myconnections.DBConnection;
 
+import java.math.BigDecimal;
 import java.sql.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 import java.util.Scanner;
+
 
 public class Gestion_Table_Employé {
 
@@ -30,7 +38,8 @@ public class Gestion_Table_Employé {
         int idBur = sc.nextInt();
         boolean ok = true;
         try {
-            PreparedStatement pstmt = dbConnect.prepareStatement("insert into apiemploye (id_employe,mail_emp,nom,prenom,id_bureau) VALUES (APIEMPLOYE_SEQ.nextval,?, ?, ?, ?)");
+            PreparedStatement pstmt = dbConnect.prepareStatement("insert into apiemploye (id_employe,mail_emp,nom,prenom,id_bureau) " +
+                    "VALUES (APIEMPLOYE_SEQ.nextval,?, ?, ?, ?)");
             pstmt.setString(1, mailEmp);
             pstmt.setString(2, nomEmp);
             pstmt.setString(3, prenomEmp);
@@ -47,7 +56,7 @@ public class Gestion_Table_Employé {
         DBConnection.closeConnection();
     }
 
-    private void select_all() {
+    public void select_all() {
         Connection dbConnect = DBConnection.getConnection();
         if (dbConnect == null) {
             System.exit(1);
@@ -55,7 +64,7 @@ public class Gestion_Table_Employé {
         System.out.println("connexion établie");
         try (Statement stmt = dbConnect.createStatement();
              ResultSet rs = stmt.executeQuery(
-                     "SELECT *  FROM APIEMPLOYE ");) {
+                     "SELECT *  FROM APIEMPLOYE ORDER BY id_employe");) {
             boolean trouve = false;
             while (rs.next()) {
                 trouve = true;
@@ -94,7 +103,7 @@ public class Gestion_Table_Employé {
         try (Statement stmt = dbConnect.createStatement();
              ResultSet rs = stmt.executeQuery(
                      "SELECT *  FROM APIEMPLOYE WHERE ID_BUREAU = '"
-                             + idBur + "'");) {
+                             + idBur + "'ORDER BY id_employe");) {
             boolean trouve = false;
             while (rs.next()) {
                 trouve = true;
@@ -116,7 +125,7 @@ public class Gestion_Table_Employé {
     }
 
 
-    private void select_by_id_emp() {
+    public void select_by_id_emp() {
         Scanner sc = new Scanner(System.in);
         Connection dbConnect = DBConnection.getConnection();
         if (dbConnect == null) {
@@ -137,9 +146,11 @@ public class Gestion_Table_Employé {
                 String nom = rs.getString("NOM");
                 String prenom = rs.getString("PRENOM");
                 String mail = rs.getString("MAIL_EMP");
-
                 int n_bur = rs.getInt("ID_BUREAU");
                 System.out.println(n_emp + ") " + nom + " " + prenom + " " + mail + " id du bureau : " + n_bur);
+                Employe em = new Employe(n_emp,mail,nom,prenom);
+                System.out.println(em);
+                infoSuppl(em);
             }
             if (!trouve) {
                 System.out.println("id de l'employé inconnu");
@@ -151,7 +162,7 @@ public class Gestion_Table_Employé {
         DBConnection.closeConnection();
     }
 
-    private void select_by_name() {
+    public void select_by_name() {
         Scanner sc = new Scanner(System.in);
         Connection dbConnect = DBConnection.getConnection();
         if (dbConnect == null) {
@@ -176,6 +187,9 @@ public class Gestion_Table_Employé {
                 String mail = rs.getString("MAIL_EMP");
                 int n_emp = rs.getInt("ID_EMPLOYE");
                 System.out.println(n_emp + " :" + nom + " " + prenom + " " + mail + " : " + idBur);
+                Employe em = new Employe(n_emp,mail,nom,prenom);
+                System.out.println(em);
+                infoSuppl(em);
             }
             if (!trouve) {
                 System.out.println("employé inconnu");
@@ -188,10 +202,27 @@ public class Gestion_Table_Employé {
     }
 
     private void update() {
-        //todo update query
+        Scanner sc = new Scanner(System.in);
+        Connection dbConnect = DBConnection.getConnection();
+        System.out.println("id du client recherché ");
+        int idemp= sc.nextInt();
+        sc.skip("\n");
+        System.out.println("nouveau email ");
+        String mail = sc.nextLine();
+        String query = "update apiemploye set mail_emp = ? WHERE id_employe = ?";
+        try(PreparedStatement pstm = dbConnect.prepareStatement(query)) {
+            pstm.setString(1,mail);
+            pstm.setInt(2,idemp);
+            int n = pstm.executeUpdate();
+            if(n!=0) System.out.println(n+ "ligne mise à jour");
+            else System.out.println("record introuvable");
+
+        } catch (SQLException e) {
+            System.out.println("erreur sql :" + e);
+        }
     }
 
-    private void delete() {
+    public void delete() {
         Scanner sc = new Scanner(System.in);
         Connection dbConnect = DBConnection.getConnection();
         if (dbConnect == null) {
@@ -221,7 +252,7 @@ public class Gestion_Table_Employé {
 
         int choix;
         String tmp;
-        String liste = "\t1.Ajout Employé\n\t2.Recherche Employé\n\t3.Modifier Employé\n\t4.Supprimé Employé\n\t5.Quitté Programme";
+        String liste = "\t1.Ajout Employé\n\t2.Recherche Employé\n\t3.Modifier Employé\n\t4.Supprimer Employé\n\t5.Quitter Programme";
         do {
             System.out.println(liste);
             System.out.println("\nQuel choix ? ");
@@ -282,7 +313,97 @@ public class Gestion_Table_Employé {
         }
     }
 
-    public static String controle(String regex, String erreur) {
+    private void infoSuppl(Employe employe){
+        int choix;
+        String tmp;
+        String liste = "\t1.Les messages envoyés\n\t2.Les messages reçu\n\t3.Les messages non lu\n\t4.Retour";
+        do {
+            System.out.println(liste);
+            System.out.println("\nQuel choix ? ");
+            tmp = controle("[1-4]", "Num entre 1 et 4 : ");
+            choix = Integer.parseInt(tmp);
+            switch (choix) {
+                case 1 -> messageSend(employe);
+                case 2 -> messageReceived(employe);
+                case 3 -> messageNotRead(employe);
+                case 4 -> System.out.println("Retour");
+                default -> System.out.println("Erreur ");
+            }
+        } while (choix != 4);
+    }
+    private void messageSend(Employe employe){
+        String query = "SELECT * FROM APIMESSAGE WHERE ID_EMPLOYE = ?";
+
+        Connection dbConnect = DBConnection.getConnection();
+        try(PreparedStatement pstm = dbConnect.prepareStatement(query)) {
+            pstm.setInt(1,employe.getId());
+            ResultSet rs = pstm.executeQuery();
+            boolean trouve= false;
+            while(rs.next()){
+                trouve=true;
+                int id_mess = rs.getInt(1);
+                String obj = rs.getString(2);
+                String contenu = rs.getString(3);
+                LocalDate dateEnvoi = rs.getDate(4).toLocalDate();
+                int id_emp = rs.getInt(5);
+
+
+                Message ms= new Message(id_mess,obj,contenu,dateEnvoi,employe) ;
+                System.out.println(ms);
+            }
+            if(!trouve) System.out.println("aucun message trouvé");
+        } catch (SQLException e) {
+            System.out.println("erreur sql :"+e);
+        }
+    }
+    private void messageReceived(Employe employe){
+        String query = "SELECT * FROM APIINFOS  WHERE ID_EMPLOYE = ?";
+
+        Connection dbConnect = DBConnection.getConnection();
+        try(PreparedStatement pstm = dbConnect.prepareStatement(query)) {
+            pstm.setInt(1,employe.getId());
+            ResultSet rs = pstm.executeQuery();
+            boolean trouve= false;
+            while(rs.next()) {
+                trouve = true;
+                int id_employe = rs.getInt(1);
+                String id_mess = rs.getString(2);
+                LocalDate date = rs.getDate(3) == null ? null : rs.getDate(3).toLocalDate();
+                String formattedDate = null;
+                if (date != null) {
+                    formattedDate = date.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG));
+                }
+                Infos i = new Infos(employe);
+                System.out.println(i + " "+ id_mess +" " + formattedDate);
+            }
+            if(!trouve) System.out.println("aucun message trouvé");
+        } catch (SQLException e) {
+            System.out.println("erreur sql :"+e);
+        }
+    }
+
+    private void messageNotRead(Employe employe){
+        String query = "SELECT * FROM APIVIEW_NOTREAD  WHERE ID_EMPLOYE = ?";
+
+        Connection dbConnect = DBConnection.getConnection();
+        try(PreparedStatement pstm = dbConnect.prepareStatement(query)) {
+            pstm.setInt(1,employe.getId());
+            ResultSet rs = pstm.executeQuery();
+            boolean trouve= false;
+            while(rs.next()) {
+                trouve = true;
+                int id_employe = rs.getInt(1);
+                String id_mess = rs.getString(2);
+                Infos i = new Infos(employe);
+                System.out.println(i + " "+ id_mess);
+            }
+            if(!trouve) System.out.println("aucun message non lu");
+        } catch (SQLException e) {
+            System.out.println("erreur sql :"+e);
+        }
+    }
+
+    private static String controle(String regex, String erreur) {
         String ctrl;
         Scanner sc = new Scanner(System.in);
         ctrl = sc.nextLine();
